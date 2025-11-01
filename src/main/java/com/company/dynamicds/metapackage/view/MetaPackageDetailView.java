ac08e63d3878dd3f2a4eb6827192a93c2be41385
Nhu Xuan Viet <nhuxuanviet27102004@gmail.com>
2025-11-01 22:28:48 +0700

    fix loi view detail

--- src/main/java/com/company/dynamicds/metapackage/view/MetaPackageDetailView.java
+++ src/main/java/com/company/dynamicds/metapackage/view/MetaPackageDetailView.java
@@ -1,16 +1,54 @@
 package com.company.dynamicds.metapackage.view;
 
+import com.company.dynamicds.dynamicds.entity.DynamicDataStoreConfig;
 import com.company.dynamicds.metapackage.entity.MetaPackage;
+import com.company.dynamicds.metapackage.entity.MetaPackageFieldMapping;
+import com.company.dynamicds.metapackage.entity.MetaPackageSource;
+import com.company.dynamicds.utils.ui.GridEditorUtils;
 import com.company.dynamicds.view.main.MainView;
+import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.router.Route;
-import io.jmix.flowui.view.EditedEntityContainer;
-import io.jmix.flowui.view.StandardDetailView;
-import io.jmix.flowui.view.ViewController;
-import io.jmix.flowui.view.ViewDescriptor;
+import io.jmix.flowui.component.combobox.JmixComboBox;
+import io.jmix.flowui.component.grid.DataGrid;
+import io.jmix.flowui.model.CollectionContainer;
+import io.jmix.flowui.model.CollectionLoader;
+import io.jmix.flowui.view.*;
 
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Detail view for MetaPackage configuration
+ * Allows editing sources and field mappings
+ */
 @Route(value = "meta-packages/:id", layout = MainView.class)
-@ViewController(id = "dwh_MetaPackage.detail")
+@ViewController(id = "MetaPackage.detail")
 @ViewDescriptor(path = "meta-package-detail-view.xml")
 @EditedEntityContainer("metaPackageDc")
 public class MetaPackageDetailView extends StandardDetailView<MetaPackage> {
+
+    @ViewComponent
+    private DataGrid<MetaPackageSource> sourcesDataGrid;
+    @ViewComponent
+    private CollectionLoader<DynamicDataStoreConfig> dynamicDataStoreConfigsDl;
+    @ViewComponent
+    private DataGrid<MetaPackageFieldMapping> fieldMappingsDataGrid;
+    @ViewComponent
+    private JmixComboBox<String> storeNameField;
+
+    @ViewComponent
+    private CollectionContainer<DynamicDataStoreConfig> dynamicDataStoreConfigsDc;
+
+    @Subscribe
+    public void onInit(final InitEvent event) {
+        dynamicDataStoreConfigsDl.load();
+        List<String> storeNames = dynamicDataStoreConfigsDc.getItems().stream()
+                .map(DynamicDataStoreConfig::getStoreName)
+                .toList();
+        storeNameField.setItems(storeNames);
+        // Setup inline editors for data grids
+        GridEditorUtils.setupInlineEditor(sourcesDataGrid);
+        GridEditorUtils.setupInlineEditor(fieldMappingsDataGrid);
+    }
+
 }
